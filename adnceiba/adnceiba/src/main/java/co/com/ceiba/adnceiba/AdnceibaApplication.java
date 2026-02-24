bfd0b82ac5c9bc9676864f12c3e7162e44dd857d
Juan Esteban García Castro <juan.garcia@ceiba.com.co>
2026-02-24 16:46:01 -0500

    feat: proyecto inicial Spring Boot

---
+++ adnceiba/adnceiba/src/main/java/co/com/ceiba/adnceiba/AdnceibaApplication.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.adnceiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdnceibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdnceibaApplication.class, args);
+	}
+
+}
---
+++ adnceiba/adnceiba/src/test/java/co/com/ceiba/adnceiba/AdnceibaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.adnceiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdnceibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
